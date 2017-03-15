e4a142a4a7e76f90cb8299ff26bf296ae21add11
jasonfish4 <jasonmjohnson1@gmail.com>
2017-03-15 13:44:15 -0400

    Create htypes.h

---
+++ Source/htypes.h
@@ -0,0 +1,28 @@
+#ifndef HTYPES_H
+#define HTYPES_H
+
+struct AddressRecord
+{
+	void* Address;
+	const char* AddressData;
+};
+
+struct HookRecord
+{
+	void* FunctionHook = NULL;
+	void* HookFunction = NULL;
+	const char* FunctionHookData;
+	unsigned char OriginBytes[5];
+};
+
+struct HookRecordEx
+{
+	void* FunctionHook = NULL;
+	void* HookFunction = NULL;
+	const char* FunctionHookData;
+	const char* ProcessName;
+	HANDLE Process;
+	unsigned char OriginBytes[5];
+};
+
+#endif
